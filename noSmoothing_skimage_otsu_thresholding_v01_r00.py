e06d9dfe05548f0098d636eeaca0f9179d0a6f02
Soraya Kaiser <soraya.kaiser@posteo.de>
2020-01-20 17:04:37 +0100

    Version 20200120

--- noSmoothing_skimage_otsu_thresholding_v01_r00.py
+++ noSmoothing_skimage_otsu_thresholding_v01_r00.py
@@ -25,8 +25,10 @@ from osgeo import gdal
 
 # get filename, sigma, and threshold value from command line
 #path_in = sys.argv[1]
-path_in = "/home/skaiser/Desktop/Code_Subs_Soraya/HERE/0_preproc_data"
-fig_out = "/home/skaiser/permamount/staff/soraya_kaiser/git2/2_plots"
+#path_in = "/home/skaiser/Desktop/Code_Subs_Soraya/HERE/0_preproc_data"
+path_in = "/home/skaiser/permamount/data/remote_sensing/HighResImagery/DigitalGlobe/ftp2.digitalglobe.com/0_preproc_data"
+#fig_out = "/home/skaiser/permamount/staff/soraya_kaiser/git2/2_plots"
+fig_out = "/home/skaiser/permamount/data/remote_sensing/HighResImagery/DigitalGlobe/ftp2.digitalglobe.com/2_plots"
 
 #%%
 
@@ -34,50 +36,50 @@ filenames = []
 input = []
 for root, dirs, files in os.walk(path_in):
     for file in files:
-        if fnmatch.fnmatch(file, '*sub.TIF'):
+        if fnmatch.fnmatch(file, '*.TIF'):
             filepath = root + '/' + file
             input.append(filepath)
             
 for i in input:
     cmd = "gdal_translate -ot Byte -of GTiff %s %s_8B.TIF" %(i, i[:-4])
-    #os.system(cmd)
+    os.system(cmd)
     filenames.append("%s_8B.TIF" %i[:-4])
 
 #%%    user defined t- value 
-
-for i in filenames:
-    image = skimage.io.imread(fname=i)   # image[rows, columns, dimensions]-> image[:,:,3] is near Infrared
-
-    nir = image[:,:,3]
-    gtif = gdal.Open(i)
-    geotransform = gtif.GetGeoTransform()
-    sourceSR = gtif.GetProjection()
-    
-    x = np.shape(image)[1]
-    y = np.shape(image)[0]
-    bands = np.shape(image)[2]
-    
- 
-   
-    # blur and grayscale before thresholding
-    blur = skimage.color.rgb2gray(nir)
-    blur = skimage.filters.gaussian(blur, sigma=2.0)
-    
-    # perform inverse binary thresholding
-    mask = blur < t
-    
-    #output np array as GeoTiff
-    file_out = '%s/%s_mask_t%s.TIF'% (path_in, i.rsplit('/')[-1][:-4], str(t))
-    dst_ds = gdal.GetDriverByName('GTiff').Create(file_out, x, y, 1, gdal.GDT_Float32)   
-    dst_ds.GetRasterBand(1).WriteArray(mask)
-    dst_ds.SetGeoTransform(geotransform)
-    dst_ds.SetProjection(sourceSR) 
-    dst_ds.FlushCache()
-    dst_ds = None
-    
-    #polygonize and write to Shapefile
-    cmd = 'gdal_polygonize.py %s -f "ESRI Shapefile" %s.shp' %(file_out, file_out[:-4])
-    os.system(cmd)
+#
+#for i in filenames:
+#    image = skimage.io.imread(fname=i)   # image[rows, columns, dimensions]-> image[:,:,3] is near Infrared
+#
+#    nir = image[:,:,3]
+#    gtif = gdal.Open(i)
+#    geotransform = gtif.GetGeoTransform()
+#    sourceSR = gtif.GetProjection()
+#    
+#    x = np.shape(image)[1]
+#    y = np.shape(image)[0]
+#    bands = np.shape(image)[2]
+#    
+# 
+#   
+#    # blur and grayscale before thresholding
+#    blur = skimage.color.rgb2gray(nir)
+#    blur = skimage.filters.gaussian(blur, sigma=2.0)
+#    
+#    # perform inverse binary thresholding
+#    mask = blur < t
+#    
+#    #output np array as GeoTiff
+#    file_out = '%s/%s_mask_t%s.TIF'% (path_in, i.rsplit('/')[-1][:-4], str(t))
+#    dst_ds = gdal.GetDriverByName('GTiff').Create(file_out, x, y, 1, gdal.GDT_Float32)   
+#    dst_ds.GetRasterBand(1).WriteArray(mask)
+#    dst_ds.SetGeoTransform(geotransform)
+#    dst_ds.SetProjection(sourceSR) 
+#    dst_ds.FlushCache()
+#    dst_ds = None
+#    
+#    #polygonize and write to Shapefile
+#    cmd = 'gdal_polygonize.py %s -f "ESRI Shapefile" %s.shp' %(file_out, file_out[:-4])
+#    os.system(cmd)
     
 #%%    Otsu
 
